fadc0f35df743f2fb8b3ce07f7d830596a516ebe
Andrew Ek <andrewek@gmail.com>
2016-04-04 14:57:52 -0500

    Query routes

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require "sinatra"
+require "pry"
 
 get '/' do
   "Hello, class!"
@@ -17,7 +18,19 @@ get '/hello/:name/:greeting' do
 end
 
 get '/letter-count/:word' do
+  binding.pry
   "#{params['word']} has #{params['word'].length} letters"
 end
+
+# /letter-count-queries?word=ruby
+get '/letter-count-queries' do
+  binding.pry
+  if params['word']
+    "#{params['word']} has #{params['word'].length} letters"
+  else
+    "You gotta give us a word."
+  end
+end
+
 
 
