b5f654c632be939de9715e1ff798da040b09ac83
Andrew Ek <andrewek@gmail.com>
2016-04-05 11:28:00 -0500

    Updated to include ERB

--- main.rb
+++ main.rb
@@ -2,35 +2,17 @@ require "sinatra"
 require "pry"
 
 get '/' do
-  "Hello, class!"
-end
-
-get '/hello' do
-  "Hello!"
+  # "Hello!" # => Before we switched to ERB
+  # render views/root_path.erb
+  erb :root_path
 end
 
 get '/hello/:name' do
-  "Hello, #{params['name'].capitalize}"
+  @name = params['name']
+  erb :hello
 end
 
-get '/hello/:name/:greeting' do
-  "#{params['greeting']} #{params['name']}"
+get '/wilhelm-scream/:num' do
+  @num = params['num'].to_i
+  erb :wilhelm_scream
 end
-
-get '/letter-count/:word' do
-  binding.pry
-  "#{params['word']} has #{params['word'].length} letters"
-end
-
-# /letter-count-queries?word=ruby
-get '/letter-count-queries' do
-  binding.pry
-  if params['word']
-    "#{params['word']} has #{params['word'].length} letters"
-  else
-    "You gotta give us a word."
-  end
-end
-
-
-
